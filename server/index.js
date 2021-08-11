9aacd6404ed9ee8e2aef86b6687bc46c51af2ccf
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-11 14:50:38 -0700

    Setup basic router on server to query all entries from test db table questions

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./routers');
+const db = require('../database/index');
 
 const HOST = '0.0.0.0';
 const PORT = 3000;
@@ -7,5 +8,11 @@ const PORT = 3000;
 const app = express();
 app.use('/hello', (req, res) => res.status(200).send('Wingardium, leviosa!\n'));
 app.use('/', router);
+
+app.use('/test', (req, res) => {
+  db('questions').select('date_written')
+    .then((rows) => rows.map((row) => row.date_written))
+    .then((dates) => res.status(200).json(dates));
+});
 
 app.listen(PORT, HOST, () => console.log(`Connected on http://${HOST}:${PORT}`));
