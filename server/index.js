45682e1bdaf9626e20cf9217b63f17c33f802ec7
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-12 15:38:47 -0700

    Incorporate CORS into server response processing, client updates

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 /* eslint-disable camelcase */
 const express = require('express');
+const cors = require('cors');
+
 const router = require('./routers/index');
 const { SERVER } = require('./config/constants');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/qa', router);
 app.use('/hello', (req, res) => res.status(200).send('Wingardium, leviosa!\n'));
 
