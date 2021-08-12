53f9426e4367e26cbfa2d332a79520fac70ce4ca
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-11 23:22:14 -0700

    Implement post question to database

--- server/controllers/postAnswer.js
+++ server/controllers/postAnswer.js
@@ -1,11 +1,28 @@
-const { GET_ANSWERS } = require('../config/constants');
-const db = require('../../database/test/index');
+const db = require('../../database/index');
 
 module.exports = (req, res) => {
-  const { body } = req;
   const { question_id } = req.params;
 
-  db('questions');
+  const {
+    body,
+    name,
+    email,
+    photos,
+  } = req.body;
 
-  res.sendStatus(201);
+  const answer = {
+    question_id,
+    body,
+    date_written: Date.now(),
+    answerer_name: name,
+    answerer_email: email,
+    reported: false,
+    helpful: 0,
+  };
+
+  db('answers').insert(answer)
+    .then(() => res.sendStatus(201))
+    .catch((error) => res.status(404).send(error));
+
+  db('photos').insert(photos);
 };
--- server/controllers/postQuestion.js
+++ server/controllers/postQuestion.js
@@ -1,4 +1,3 @@
-const { GET_ANSWERS } = require('../config/constants');
 const db = require('../../database/index');
 
 module.exports = (req, res) => {
