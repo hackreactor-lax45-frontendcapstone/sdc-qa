9e89eaa5498f846458606e96b2101e3eb1439e3f
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-12 19:10:16 -0700

    Implement various testing for simultaneous queries to the service (4 requests, then 100 requests)

--- spec/server.test.js
+++ spec/server.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-undef */
 const axios = require('axios');
 
@@ -19,7 +20,7 @@ describe('GET questions from single product', () => {
       .finally(() => done());
   });
 
-  test(`response time less than ${FLIGHT_TIME}ms`, (done) => {
+  test(`response time < ${FLIGHT_TIME}ms`, (done) => {
     const START = Date.now();
     axios.get(QUESTIONS, { params: { product_id: PRODUCT_ID } })
       .then(() => {
@@ -56,7 +57,7 @@ describe('GET answers from single question', () => {
       .finally(() => done());
   });
 
-  test(`response time less than ${FLIGHT_TIME}ms`, (done) => {
+  test(`response time < ${FLIGHT_TIME}ms`, (done) => {
     const START = Date.now();
     axios.get(`${QUESTIONS}/${QUESTION_ID}/answers`)
       .then(() => {
@@ -71,6 +72,130 @@ describe('GET answers from single question', () => {
       .then((response) => {
         expect(response.data.question).toEqual(QUESTION_ID.toString());
         expect(response.data.results[0].answer_id).toEqual(ANSWER_ID);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => done());
+  });
+});
+
+describe('GET questions from a series of products', () => {
+  const STATUS = 200; // OK
+  const FLIGHT_TIME = 100; // ms
+
+  // If fail, manually query DB: SELECT MAX(product_id) FROM questions;
+  const MAX_ID = 1000011;
+  const PRODUCT_IDS = [];
+  const LATE_PRODUCT_IDS = [];
+  const TEST_COUNT = 10;
+  for (let i = 0; i < TEST_COUNT; i += 1) {
+    PRODUCT_IDS.push(Math.floor(Math.random() * MAX_ID));
+    LATE_PRODUCT_IDS.push(Math.floor((0.9 + 0.1 * Math.random()) * MAX_ID));
+  }
+
+  test(`status code ${STATUS} for ${TEST_COUNT} random products`, (done) => {
+    const queries = PRODUCT_IDS.map((product_id) => axios.get(
+      QUESTIONS, { params: { product_id } },
+    )
+      .then((response) => response.status)
+      .catch((error) => { throw error; }));
+
+    Promise.all(queries)
+      .then((results) => {
+        results.forEach((result) => expect(result).toEqual(STATUS));
+      })
+      .catch((error) => console.error(error))
+      .finally(() => done());
+  });
+
+  test(`response time < ${FLIGHT_TIME}ms for 4 random products`, (done) => {
+    const PRODUCT_RESPONSES = [];
+
+    let START = Date.now();
+    let INDEX = 0;
+    PRODUCT_RESPONSES[INDEX] = axios.get(QUESTIONS, { params: { product_id: PRODUCT_IDS[INDEX] } })
+      .then(() => Date.now() - START)
+      .catch((error) => { throw error; });
+
+    START = Date.now();
+    INDEX += 1;
+    PRODUCT_RESPONSES[INDEX] = axios.get(QUESTIONS, { params: { product_id: PRODUCT_IDS[INDEX] } })
+      .then(() => Date.now() - START)
+      .catch((error) => { throw error; });
+
+    START = Date.now();
+    INDEX += 1;
+    PRODUCT_RESPONSES[INDEX] = axios.get(QUESTIONS, { params: { product_id: PRODUCT_IDS[INDEX] } })
+      .then(() => Date.now() - START)
+      .catch((error) => { throw error; });
+
+    START = Date.now();
+    INDEX += 1;
+    PRODUCT_RESPONSES[INDEX] = axios.get(QUESTIONS, { params: { product_id: PRODUCT_IDS[INDEX] } })
+      .then(() => Date.now() - START)
+      .catch((error) => { throw error; });
+
+    Promise.all(PRODUCT_RESPONSES)
+      .then((results) => {
+        console.log('fully random: ', results);
+        results.forEach((result) => expect(result).toBeLessThan(FLIGHT_TIME));
+      })
+      .catch((error) => console.error(error))
+      .finally(() => done());
+  });
+
+  test(`response time < ${FLIGHT_TIME}ms for 4 random products in last '10%' of table`, (done) => {
+    const PRODUCT_RESPONSES = [];
+
+    let START = Date.now();
+    let INDEX = 0;
+    PRODUCT_RESPONSES[INDEX] = axios.get(QUESTIONS,
+      { params: { product_id: LATE_PRODUCT_IDS[INDEX] } })
+      .then(() => Date.now() - START)
+      .catch((error) => { throw error; });
+
+    START = Date.now();
+    INDEX += 1;
+    PRODUCT_RESPONSES[INDEX] = axios.get(QUESTIONS,
+      { params: { product_id: LATE_PRODUCT_IDS[INDEX] } })
+      .then(() => Date.now() - START)
+      .catch((error) => { throw error; });
+
+    START = Date.now();
+    INDEX += 1;
+    PRODUCT_RESPONSES[INDEX] = axios.get(QUESTIONS,
+      { params: { product_id: LATE_PRODUCT_IDS[INDEX] } })
+      .then(() => Date.now() - START)
+      .catch((error) => { throw error; });
+
+    START = Date.now();
+    INDEX += 1;
+    PRODUCT_RESPONSES[INDEX] = axios.get(QUESTIONS,
+      { params: { product_id: LATE_PRODUCT_IDS[INDEX] } })
+      .then(() => Date.now() - START)
+      .catch((error) => { throw error; });
+
+    Promise.all(PRODUCT_RESPONSES)
+      .then((results) => {
+        console.log('last \'10%\' random: ', results);
+        results.forEach((result) => expect(result).toBeLessThan(FLIGHT_TIME));
+      })
+      .catch((error) => console.error(error))
+      .finally(() => done());
+  });
+
+  test(`response time < ${200}ms for ${TEST_COUNT} random products in last '10%' of table`, (done) => {
+    let START;
+    const PRODUCT_RESPONSES = LATE_PRODUCT_IDS.map((product_id) => {
+      START = Date.now();
+      return axios.get(QUESTIONS, { params: { product_id } })
+        .then(() => Date.now() - START)
+        .catch((error) => { throw error; });
+    });
+
+    Promise.all(PRODUCT_RESPONSES)
+      .then((results) => {
+        console.log(results);
+        results.forEach((result) => expect(result).toBeLessThan(200));
       })
       .catch((error) => console.error(error))
       .finally(() => done());
