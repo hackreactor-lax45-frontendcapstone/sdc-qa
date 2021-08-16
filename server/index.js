d33a76a5d383022cb346b19da8bfb2e36b976ecf
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-16 10:19:59 -0700

    Incorporate clustering into server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 /* eslint-disable camelcase */
 require('newrelic');
 const express = require('express');
+const cluster = require('cluster');
 const cors = require('cors');
 const path = require('path');
+
+const CPUs = require('os').cpus().length;
+const process = require('process');
 
 const router = require('./routers/index');
 const { SERVER } = require('./config/constants');
@@ -15,4 +19,20 @@ app.use('/qa', router);
 app.use('/hello', (req, res) => res.status(200).send('Wingardium, leviosa!\n'));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(SERVER.PORT, SERVER.HOST, () => console.log(`Connected on http://${SERVER.HOST}:${SERVER.PORT}`));
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers
+  for (let i = 0; i < CPUs; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on('online', (worker) => {
+    console.log(`Worker ${worker.process.pid} is online.`);
+  });
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died.`);
+  });
+} else {
+  app.listen(SERVER.PORT, SERVER.HOST, () => console.log(`Connected on http://${SERVER.HOST}:${SERVER.PORT}`));
+}
