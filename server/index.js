af1e9e98616463ef1c20ff888ba79482bfd40076
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-16 10:18:41 -0700

    Incorporate loaderio verification

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 require('newrelic');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const router = require('./routers/index');
 const { SERVER } = require('./config/constants');
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use(cors());
 app.use('/qa', router);
 app.use('/hello', (req, res) => res.status(200).send('Wingardium, leviosa!\n'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(SERVER.PORT, SERVER.HOST, () => console.log(`Connected on http://${SERVER.HOST}:${SERVER.PORT}`));
